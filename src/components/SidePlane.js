28f7bf00d243232ea30a085c9479504ea6fda35a
geoffrey1330 <israelgeoffrey13@gmail.com>
2021-03-17 11:12:50 +0100

    Sidebar component

--- src/components/SidePlane.js
+++ src/components/SidePlane.js
@@ -1,5 +1,47 @@
 import React from "react";
 
-export default function SidePlane() {
-  return <div>SidePlane component</div>;
+export default function SidePlane({SetType}) {
+
+  return(
+    <div>
+      <h1>SidePlane Component</h1>
+      <button onClick={() => {
+         SetType("BarChart");
+        }}>Barchart</button>
+        {" "}
+         <button onClick={() => {
+         SetType("LineChart");
+        }}>Linechart</button>
+         {" "}
+         <button onClick={() => {
+         SetType("PieChart");
+        }}
+         >Piechart</button>
+    </div>
+      
+  )
 }
+
+ /* const onChange = e => SetType({[e.target.name]: e.target.value });
+
+  const{chart}=type;
+  
+  const onSubmit = e => {
+    e.preventDefault();
+    //SetType("BarChart");    
+};
+  return (
+        <form onSubmit={e => onSubmit(e)}>
+          <div>
+          <label>chartType</label>
+          <select name='chart' onChange={e => onChange(e)} value={chart}>
+              <option>BarChart</option>
+              <option>LineChart</option>
+              <option>PieChart</option>
+          </select>
+          <button>UPDATE</button>
+          </div>
+
+        </form>
+  )
+}*/
--- src/containers/ChartPage.js
+++ src/containers/ChartPage.js
@@ -6,8 +6,8 @@ function ChartPage(props) {
     return (
         <>
             <div  className="grid grid-cols-2 gap-10 px-5 py-20 sm:grid-cols-2 sm:gap-1 xl:px-40">
-                <Chart dataset={props.data}/>
-                <SidePlane/>
+                <Chart dataset={props.data} type={props.type}/>
+                <SidePlane SetType={props.SetType}/>
             </div>
         </>
     )
