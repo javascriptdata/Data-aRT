84675d9bebd5a292b0ac3681ecf53759d84799de
geoffrey1330 <israelgeoffrey13@gmail.com>
2021-04-07 15:36:16 +0100

    'chart_draggable

--- src/containers/ChartPage.js
+++ src/containers/ChartPage.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Chart from "../components/Chart";
 import SidePlane from "../components/SidePlane";
+import Draggable from 'react-draggable';
 
 function ChartPage(props) {
     if(props.display==="Chart"){
     return (
         <>
-            <div  className="grid grid-cols-2 gap-10 px-5 py-20 sm:grid-cols-2 sm:gap-1 xl:px-40">
+            <div className="grid grid-cols-2 gap-10 px-5 py-20 sm:grid-cols-2 sm:gap-1 xl:px-40">
                 <Chart dataset={props.data} type={props.type}/>
                 <SidePlane SetType={props.SetType}/>
             </div>
