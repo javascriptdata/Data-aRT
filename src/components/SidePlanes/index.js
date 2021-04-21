a275010ca4afff6f320df52dd509fa4c24a7dcff
steveoni <steohenoni2@gmail.com>
2021-04-21 18:59:36 +0000

    [index.js][s]: export datframe 2 dataframe component

--- src/components/SidePlanes/index.js
+++ src/components/SidePlanes/index.js
@@ -1,5 +1,6 @@
 import Describe from './Describe'
 import Query from './Query'
 import Arithemtic from './Arithemtic'
+import Df2df from './Df2df'
 
-export { Describe, Query, Arithemtic }
+export { Describe, Query, Arithemtic, Df2df }
