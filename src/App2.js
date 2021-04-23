6bfbb9b329e1cbb27a752f5f765198ed1179cccc
steveoni <steohenoni2@gmail.com>
2021-04-23 00:21:09 +0000

    [App2.js][l]: full implement the sideplane, char component

--- src/App2.js
+++ src/App2.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import DataTables from './components/DataTables';
-import { Df2df } from './components/SidePlanes'
+import { SidePlane } from './components/SidePlanes'
+import ChartsViz from './components/ChartsViz'
+import ChartPlane from './components/ChartPlane'
 import { read_csv } from 'danfojs/src/io/reader'
 
 function App2() {
-  // console.log(tf)
   const [file, setFile ] = useState()
   const [dataComp, setDataComp] = useState([])
   const [showSidePlane, setSidePlane] = useState(false)
   const [compIndex, setCompIndex] = useState()
+  const [chartType, setChartType] = useState()
+  const [dfOpsType, setDfOpsType] = useState()
+  const [chartComp, setChartComp] = useState([])
 
   const changeHandler = function(event) {
       const content = event.target.files[0]
@@ -35,11 +39,53 @@ function App2() {
       })
   }
 
+  const charts = ["BarChart", "LineChart", "PieChart"]
+  const dataFrameOps = ["Arithemtic","Describe", "Df2df", "Query"]
+
+  const handleChart = (e) => {
+    const value = e.target.innerHTML
+
+    setChartType(value)
+    setSidePlane("chart")
+  }
+
+  const handleDfops = (e) =>{
+    const value = e.target.value
+    
+    setDfOpsType(value)
+    setSidePlane("datatable")
+    
+  }
+
   return (
     <div className="max-w-full mx-auto border-2 mt-10">
       <div className="flex flex-col">
-        <div className="border-2 w-1/2 mb-10">
+        <div className="border-2 mb-10 flex flex-row">
           <input type="file" name="file" onChange={changeHandler} />
+          <div className="mr-4">
+            {
+              charts.map((chart, i)=>{
+                return <button 
+                          className="bg-blue-500 p-2 text-white rounded-sm mr-2"
+                          onClick={handleChart}
+                      >
+                        {chart}
+                      </button>
+              })
+            }
+          </div>
+          <div>
+            <span className="mr-2">DataFrame Operations</span>
+            <select className="border" onChange={handleDfops}>
+              <option value="None" >None</option>
+              {
+                dataFrameOps.map((dfops, i)=>{
+                    return <option value={dfops} key={i}>{dfops}</option>
+                })
+              }
+            </select>
+          </div>
+
         </div>
         <div className="flex flex-row justify-between border-2">
           <div className="border-2 w-full">
@@ -51,18 +97,38 @@ function App2() {
                     setCompIndex={setCompIndex}
                 /> 
               }
+              
+              { (chartComp.length > 0) && 
+                <ChartsViz
+                    chartComp={chartComp}
+                /> 
+              }
             </div>
             
           </div>
-          {showSidePlane && 
+          {showSidePlane 
+            && 
+            (
+              showSidePlane === "datatable" ? 
+              <div className="border-2 w-1/3">
+                <SidePlane
+                  dataComp={dataComp[compIndex]} 
+                  dataComps={dataComp}
+                  df_index={compIndex}
+                  setDataComp={setDataComp}
+                  dfOpsType={dfOpsType}
+                />
+            </div> :
             <div className="border-2 w-1/3">
-              <Df2df 
-                dataComp={dataComp[compIndex]} 
-                dataComps={dataComp}
-                df_index={compIndex}
-                setDataComp={setDataComp}
-               />
+              <ChartPlane
+                  dataComp={dataComp[compIndex]}
+                  setChartComp={setChartComp}
+                  chartType={chartType}
+              />
             </div>
+
+            )
+            
             
           }
         </div>
