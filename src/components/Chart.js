e5b2a52a0833f41d88072a3794f12fbb512c8eb8
steveoni <steohenoni2@gmail.com>
2021-04-23 00:21:40 +0000

    [Chart.js][s]: update to be more generic

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -2,12 +2,13 @@ import React, {useState} from "react";
 import { Bar as BarChart } from 'react-chartjs-2';
 import { Line as LineChart } from "react-chartjs-2";
 import { Pie as PieChart} from "react-chartjs-2";
+import Draggable from 'react-draggable';
 
 
-export default function Chart({dataset,type}) {
+export default function Chart({labels, dataset,type}) {
   //let type= "BarChart";
   let data = {
-    labels: dataset.labels,
+    labels: labels,
     datasets: [{
       label: "My First dataset",
       backgroundColor: [
@@ -27,7 +28,7 @@ export default function Chart({dataset,type}) {
         'rgba(255, 159, 64, 1)'
       ],
       borderWidth:1,
-      data: dataset.dataset,
+      data: dataset,
     }]
   };
   let options = {
@@ -42,26 +43,35 @@ export default function Chart({dataset,type}) {
   }
   if(type==="BarChart"){
     return(
-      <div>
-         <BarChart data={data} options={options} width="1000" height="2000" />
+      <Draggable>
+        <div className="max-w-md">
+         <BarChart data={data} options={options} width="100" height="100" />
       </div>
+      </Draggable>
+      
     )
   }
   else if(type==="LineChart"){
     return(
-      <div>
-        <LineChart data={data} options={options} width="1000" height="2000" />
+      <Draggable>
+        <div className="max-w-md">
+        <LineChart data={data} options={options} width="100" height="100"/>
        
       </div>
+      </Draggable>
+      
     )
   }
   
   else if(type==="PieChart"){
     return(
-      <div>
-         <PieChart data={data} options={options} width="1000" height="2000" />
+      <Draggable>
+        <div className="max-w-md">
+          <PieChart data={data} options={options} width="100" height="100" />
        
-      </div>
+        </div>
+      </Draggable>
+      
     )
   }
   return(
